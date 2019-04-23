b30fd21ce7a240c2e084b3b78b082b67669177ed
Cezary Miącz <cezarymiacz@cezarys-mbp.lan>
2019-04-23 15:37:49 +0200

    Replace celery @shared_task with @app.task

--- saleor/order/emails.py
+++ saleor/order/emails.py
@@ -1,8 +1,8 @@
-from celery import shared_task
 from django.conf import settings
 from django.urls import reverse
 from templated_email import send_templated_mail
 
+from ..celeryconf import app
 from ..core.emails import get_email_base_context
 from ..core.utils import build_absolute_uri
 from ..seo.schema.email import get_order_confirmation_markup
@@ -51,28 +51,28 @@ def collect_data_for_fullfillment_email(order_pk, template, fulfillment_pk):
     return email_data
 
 
-@shared_task
+@app.task
 def send_order_confirmation(order_pk):
     """Sends order confirmation email."""
     email_data = collect_data_for_email(order_pk, CONFIRM_ORDER_TEMPLATE)
     send_templated_mail(**email_data)
 
 
-@shared_task
+@app.task
 def send_fulfillment_confirmation(order_pk, fulfillment_pk):
     email_data = collect_data_for_fullfillment_email(
         order_pk, CONFIRM_FULFILLMENT_TEMPLATE, fulfillment_pk)
     send_templated_mail(**email_data)
 
 
-@shared_task
+@app.task
 def send_fulfillment_update(order_pk, fulfillment_pk):
     email_data = collect_data_for_fullfillment_email(
         order_pk, UPDATE_FULFILLMENT_TEMPLATE, fulfillment_pk)
     send_templated_mail(**email_data)
 
 
-@shared_task
+@app.task
 def send_payment_confirmation(order_pk):
     """Sends payment confirmation email."""
     email_data = collect_data_for_email(order_pk, CONFIRM_PAYMENT_TEMPLATE)
