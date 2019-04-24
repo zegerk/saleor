9fb7f1e4fc5f253c23239704a4d8349eaf7aab13
dominik-zeglen <flesz3@o2.pl>
2019-04-24 18:09:51 +0200

    Add menu create

--- saleor/static/dashboard-next/storybook/config.js
+++ saleor/static/dashboard-next/storybook/config.js
@@ -72,6 +72,7 @@ function loadStories() {
   require("./stories/home/HomePage");
 
   // Navigation
+  require("./stories/navigation/MenuCreateDialog");
   require("./stories/navigation/MenuListPage");
 
   // Staff
